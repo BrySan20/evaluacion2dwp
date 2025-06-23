03153a624824b25bb61a1ed8ebb0964c6ced1114
BrySan20 <bryansanchez1680@gmail.com>
2025-06-23 17:36:42 -0600

    project creation

---
+++ src/test/java/mx/edu/uteq/idgs09/eval2/Eval2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs09.eval2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eval2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
